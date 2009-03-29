a60203b7687fcc9eeae8be5f73631261969617a4
AlastairDewar <Alastair@alastairdewar.co.uk>
2009-03-29 18:46:48 +0100

    Added the basics from the Foxes and Rabbits project. Also includes an implementation of traps which works as intended, but it could do with some more or adding more every say 5/10 steps

---
+++ Objects.java
@@ -0,0 +1,95 @@
+import java.util.List;
+
+/**
+ * A class representing shared characteristics of objects.
+ * 
+ * @author Alastair Fraser Dewar
+ * @version 2009.03.27
+ */
+public abstract class Objects
+{
+    // Whether the object has been triggered or not.
+    private boolean triggered;
+    // The objects's field.
+    private Field field;
+    // The objects's position in the field.
+    private Location location;
+    
+    /**
+     * Create a new object at location in field.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Objects(Field field, Location location)
+    {
+        triggered = false;
+        this.field = field;
+        setLocation(location);
+    }
+    
+    /**
+     * Make this object act - that is: make it do
+     * whatever it wants/needs to do.
+     * @param newObjects A list to add newly created objects to.
+     */
+    abstract public void act(Animal animal);
+
+    /**
+     * Check whether the object is triggered or not.
+     * @return true if the object has been triggered.
+     */
+    public boolean isTriggered()
+    {
+        return triggered;
+    }
+
+    /**
+     * Indicate that the object is to be destroyed.
+     * It is removed from the field.
+     */
+    public void destroy()
+    {
+        if(location != null) {
+            field.clear(location);
+            location = null;
+            field = null;
+        }
+    }
+
+    /**
+     * Return the object's location.
+     * @return The object's location.
+     */
+    public Location getLocation()
+    {
+        return location;
+    }
+    
+    /**
+     * Return the object's field.
+     * @return The object's field.
+     */
+    public Field getField()
+    {
+        return field;
+    }
+    
+    /**
+     * Place the object at the new location in the given field.
+     * @param newLocation The object's new location.
+     */
+    public void setLocation(Location newLocation)
+    {
+        if(location != null) {
+            field.clear(location);
+        }
+        location = newLocation;
+        field.place(this, newLocation);
+    }
+    
+    public void trigger()
+    {
+    	this.triggered = true;
+    }
+}
---
+++ Trap.java
@@ -0,0 +1,52 @@
+import java.util.List;
+
+/**
+ * A simple model of a trap.
+ * Traps eat rabbits and foxes.
+ * 
+ * @author Alastair F Dewar
+ * @version 2009.03.27
+ */
+public class Trap extends Objects
+{
+    // Characteristics shared by all traps (static fields).
+    
+	/* None */
+    
+    // Individual characteristics (instance fields).
+	
+    // Whether or not the trap is snared.
+    private boolean snared;
+
+    /**
+     * Create a trap.
+     * 
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     */
+    public Trap(Field field, Location location)
+    {
+        super(field, location);
+
+    }
+    
+    /**
+     * This is what the trap does most of the time: it just 
+     * waits for itself to be snared.
+     * @param newTraps A list to add newly added traps to.
+     */
+    public void act(Animal animal)
+    {
+        if(isTriggered()) {
+            this.snared = true;
+            animal.setDead();
+            this.destroy();
+        }
+    }
+    
+    public boolean isSnared()
+    {
+    	return snared;
+    }
+
+}
